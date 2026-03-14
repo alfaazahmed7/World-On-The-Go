8c3b8e98862f0fa4034c7c3be836605fb330a4c9
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-03-15 01:40:37 +0600

    added toggle button with conditional background based on text

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Country.css";
 
 const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false);
     console.log(country);
 
     const handleVisited = () => {
-        console.log("button clicked");
+        // // system - 01
+        // if (visited) {
+        //     setVisited(false)
+        // }
+        // else {
+        //     setVisited(true)
+        // }
+
+        // // system - 02
+        // setVisited(visited ? false : true);
+
+        // system - 03
+        setVisited(!visited)
     }
 
     return (
-        <div className='country'>
+        <div className= {`country ${visited && "visited-country"}`}>
             <h2>Name: {country.name.common}</h2>
             <h2>Population: {country.population.population}</h2>
             <h5>Area: {country.area.area}
                 {country.area.area > 300000 ? "(Big Country)" : "(Small Country)"}
             </h5>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
